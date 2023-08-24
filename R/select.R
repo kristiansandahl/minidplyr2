f6394f1a57589450e20b5c214089a7dab6e9fe5a
kristiansandahl <131251336+kristiansandahl@users.noreply.github.com>
2023-08-24 10:43:26 +0200

    first function

---
+++ R/select.R
@@ -0,0 +1,15 @@
+#' Subset a column of dataframes
+#'
+#' @param x A dataframe
+#' @param var Either a vector of column indices, or variable names or logicals
+#'
+#' @return A subset of a dataframe
+#' @export
+#'
+#' @examples
+#' select(iris, 1:2)
+#' select(iris, "Species)
+#' 
+select <- function(x, var) {
+    x[var]
+}
---
+++ R/test.R
@@ -0,0 +1,19 @@
+#' Add two numbers
+#'
+#'
+#' This is a description
+#'
+#'
+#' @param x A numeric
+#' @param y A numeric
+#'
+#' @return The sum
+#' @export 
+#'
+#' @examples
+#' add(1,2)
+#' add(3:4,5:6)
+#' 
+add <- function(x,y) {
+    x + y
+}
